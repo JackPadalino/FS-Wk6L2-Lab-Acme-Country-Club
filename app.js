22fe954881a9ad83f51c326850660931529a1ea1
JackPadalino <pada0867@gmail.com>
2022-10-03 20:33:03 -0400

    updated app.js using router

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require('express');
-//const methodOverride = require('method-override');
 const app = express();
-const bookingRouter = require('./api/booking');
-const facilityRouter = require('./api/facility');
-const memberRouter = require('./api/member');
 
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(__dirname + '/public'));
-//app.use(methodOverride('_method'));
-app.use('/bookings',bookingRouter);
-app.use('/facilities',facilityRouter);
-app.use('/members',memberRouter);
+const apiRouter = require('./api');
+app.use('/api',apiRouter);
 
 const PORT = 3000;
 
